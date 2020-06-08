35b3a523bba5beb46c3abf81ac176caefca06fa5
ryancmoniz <ryancmoniz@gmail.com>
2020-06-08 14:41:24 -0400

    Add route controllers for handle and hashtag requests

---
+++ server/routes/getPersonalityByHashtag.js
@@ -0,0 +1,32 @@
+const { generateInputTextByHashtag } = require('../../api/twitter.js');
+const { generatePersonalityProfile } = require('../../api/watson.js');
+const { getBigFive } = require('../../helpers/getBigFive.js');
+const { getNeeds } = require('../../helpers/getNeeds.js');
+const { getValues } = require('../../helpers/getValues.js');
+
+const getPersonalityByHashtag = (req, res) => {
+  const results = [];
+  const { hashtag } = req.body;
+  const checkWord = new RegExp('^[a-zA-Z0-9]*$');
+
+  if (checkWord.test(hashtag)) {
+    generateInputTextByHashtag(hashtag)
+      .then((input) => generatePersonalityProfile(input))
+      .then((profile) => {
+        results.push(getBigFive(profile.personality));
+        results.push(getNeeds(profile.needs));
+        results.push(getValues(profile.values));
+        res.status(200).send(results);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error generating profile');
+      });
+  } else {
+    res.status(500).send('Invalid input');
+  }
+};
+
+module.exports = {
+  getPersonalityByHashtag,
+};
