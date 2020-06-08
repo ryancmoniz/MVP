bf9a558776f978b8ab74ff7826541a3f3491dea5
ryancmoniz <ryancmoniz@gmail.com>
2020-06-08 13:40:29 -0400

    Create function to generate input text for group of people based on a hashtag

--- api/twitter.js
+++ api/twitter.js
@@ -4,7 +4,7 @@ const { config } = require('../secret/keys.js');
 
 const urlPattern = /([a-z]+:\/+)([^/\s]*)([a-z0-9\-@^=%&;/~+]*)[?]?([^ #]*)#?([^ #]*)/ig;
 
-const generateInputText = async (screename) => {
+const generateInputTextByHandle = async (screename) => {
   let output = '';
   const data = [];
   for (let i = 1; i <= 3; i += 1) {
@@ -25,6 +25,31 @@ const generateInputText = async (screename) => {
   return output.replace(urlPattern, '');
 };
 
+
+const generateInputTextByHashtag = async (hashtag, type = 'mixed') => {
+  let output = '';
+  let maxId = Infinity;
+  const data = [];
+  while (data.length <= 200) {
+    const url = `https://api.twitter.com/1.1/search/tweets.json?q=${hashtag}&result_type=${type}&count=100&tweet_mode=extended&max_id=${maxId}}`;
+
+    try {
+      const response = await axios.get(url, config);
+      // eslint-disable-next-line prefer-destructuring
+      maxId = response.data.search_metadata.next_results.split('max_id=')[1].split('&')[0];
+      data.push(...response.data.statuses);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  data.forEach((tweet) => {
+    output += tweet.full_text;
+  });
+
+  return output.replace(urlPattern, '');
+};
+
 module.exports = {
-  generateInputText,
+  generateInputTextByHandle,
+  generateInputTextByHashtag,
 };
