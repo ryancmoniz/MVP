a14b16e2785ac938b675c8a4e77d543d33621044
ryancmoniz <ryancmoniz@gmail.com>
2020-06-07 22:31:04 -0400

    Create function to format personality needs

---
+++ helpers/getNeeds.js
@@ -0,0 +1,22 @@
+const getNeeds = (personalityNeeds) => {
+  const needs = {
+    challenge: personalityNeeds[0].percentile,
+    closesness: personalityNeeds[1].percentile,
+    curiosity: personalityNeeds[2].percentile,
+    excitement: personalityNeeds[3].percentile,
+    harmony: personalityNeeds[4].percentile,
+    ideal: personalityNeeds[5].percentile,
+    liberty: personalityNeeds[6].percentile,
+    love: personalityNeeds[7].percentile,
+    practicality: personalityNeeds[8].percentile,
+    selfExpression: personalityNeeds[9].percentile,
+    stability: personalityNeeds[10].percentile,
+    structure: personalityNeeds[11].percentile,
+  };
+
+  return needs;
+};
+
+module.exports = {
+  getNeeds,
+};
