d5e6ec6ff9191eaeba563dc819cf5d10395c171f
ryancmoniz <ryancmoniz@gmail.com>
2020-06-07 22:19:28 -0400

    Create function to format big 5 personality characteristics

---
+++ helpers/getBigFive.js
@@ -0,0 +1,73 @@
+// const { generateInputText } = require('./api/twitter.js');
+// const { generatePersonalityProfile } = require('./api/watson.js');
+const sampleData = require('../samples/sampleWatsonData.json').result;
+
+const getBigFive = (personalityProfile) => {
+  const Openness = personalityProfile[0];
+  const Conscientousness = personalityProfile[1];
+  const Extraversion = personalityProfile[2];
+  const Agreeableness = personalityProfile[3];
+  const EmotionalRange = personalityProfile[4];
+
+  const bigFive = {
+    openness: {
+      score: Openness.percentile,
+      factors: {
+        adventurousness: Openness.children[0].percentile,
+        artisticInterests: Openness.children[1].percentile,
+        emotionality: Openness.children[2].percentile,
+        imagination: Openness.children[3].percentile,
+        intellect: Openness.children[4].percentile,
+        authorityChallenging: Openness.children[5].percentile,
+      },
+    },
+    conscientousness: {
+      score: Conscientousness.percentile,
+      factors: {
+        achievementStriving: Conscientousness.children[0].percentile,
+        cautiousness: Conscientousness.children[1].percentile,
+        dutifulness: Conscientousness.children[2].percentile,
+        orderliness: Conscientousness.children[3].percentile,
+        selfDiscipine: Conscientousness.children[4].percentile,
+        selfEfficacy: Conscientousness.children[5].percentile,
+      },
+    },
+    extraversion: {
+      score: Extraversion.percentile,
+      factors: {
+        activityLevel: Extraversion.children[0].percentile,
+        assertiveness: Extraversion.children[1].percentile,
+        cheerfulness: Extraversion.children[2].percentile,
+        excitmentSeeking: Extraversion.children[3].percentile,
+        outgoing: Extraversion.children[4].percentile,
+        gregariousness: Extraversion.children[5].percentile,
+      },
+    },
+    agreeableness: {
+      score: Agreeableness.percentile,
+      factors: {
+        altruism: Agreeableness.children[0].percentile,
+        cooperation: Agreeableness.children[1].percentile,
+        modesty: Agreeableness.children[2].percentile,
+        uncompromising: Agreeableness.children[3].percentile,
+        sympathy: Agreeableness.children[4].percentile,
+        trust: Agreeableness.children[5].percentile,
+      },
+    },
+    emotionalRange: {
+      score: EmotionalRange.percentile,
+      factors: {
+        fiery: EmotionalRange.children[0].percentile,
+        anxiety: EmotionalRange.children[1].percentile,
+        melancholy: EmotionalRange.children[2].percentile,
+        immoderation: EmotionalRange.children[3].percentile,
+        selfConsciousness: EmotionalRange.children[4].percentile,
+        vulnerability: EmotionalRange.children[5].percentile,
+      },
+    },
+  };
+
+  return bigFive;
+};
+
+console.log(getBigFive(sampleData.personality));
