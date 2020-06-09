663db0437fd20dc60f1543f3ac02625cd8f665f9
ryancmoniz <ryancmoniz@gmail.com>
2020-06-08 22:41:55 -0400

    Add component to render followers, following, total tweets

---
+++ client/src/TwitterData.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const TwitterData = ({profile = {}}) => (
+  <div className="data">
+    <div className="inner-data">
+      {console.log(profile)}
+      <div className="data-content">
+        <p>Tweets</p>
+        <span>{profile.statuses_count}</span>
+      </div>
+      <div className="data-content">
+        <p>Following</p>
+        <span>{profile.friends_count}</span>
+      </div>
+      <div className="data-content">
+        <p>Followers</p>
+        <span>{profile.followers_count}</span>
+      </div>
+    </div>
+  </div>
+);
+
+export default TwitterData;
