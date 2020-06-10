adc3185126ab1bf15c10967a06d8bac635879a6e
ryancmoniz <ryancmoniz@gmail.com>
2020-06-10 11:34:44 -0400

    Configure for heroku deployment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.post('/personality/hashtag/', getPersonalityByHashtag);
 app.post('/personality/handle/', getPersonalityByHandle);
